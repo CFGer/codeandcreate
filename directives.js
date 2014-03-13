67548a8606107c76c1f74bd3cd083ceaacc7c3f2
Nicholas Angeli <nicangeli@gmail.com>
2014-03-13 10:40:32 +0000

    init commit

---
+++ directives.js
@@ -0,0 +1,23 @@
+app.directive('item', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            title: "@"
+        },
+        template: '<input id="{{title}}" class="css-checkbox" type="checkbox" ng-click="change(title)"><label for="{{title}}" class="css-label">{{title}}</label><br />',
+        link: function(scope, element, attrs) {
+            scope.$parent.add(scope.title);
+            scope.change = function(title) {
+                scope.$parent.change(scope.title);
+            }
+        },   
+    }
+});
+
+app.directive('tasks', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        template: '<div class="alert alert-info" ng-transclude></div>'
+    }
+})
